64b4e64ca4926d83eb038ee9c2094c445575f2a3
Jonas Hietala <mail@jonashietala.se>
2021-06-10 11:16:54 +0200

    Typed schemas to reduce boilerplate

--- lib/bitpal_schemas/currency.ex
+++ lib/bitpal_schemas/currency.ex
@@ -1,20 +1,11 @@
 defmodule BitPalSchemas.Currency do
-  use Ecto.Schema
+  use TypedEctoSchema
   alias BitPalSchemas.Address
   alias BitPalSchemas.Invoice
 
-  @type id :: atom | String.t()
-
-  @type t :: %__MODULE__{
-          id: id,
-          block_height: non_neg_integer,
-          addresses: [Address.t()],
-          invoices: [Invoice.t()]
-        }
-
   @primary_key {:id, :string, []}
-  schema "currencies" do
-    field(:block_height, :integer)
+  typed_schema "currencies" do
+    field(:block_height, :integer) :: non_neg_integer | nil
     has_many(:addresses, Address)
     has_many(:invoices, Invoice)
     has_many(:tx_outputs, through: [:addresses, :tx_outputs])
