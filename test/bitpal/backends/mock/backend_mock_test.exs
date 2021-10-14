3427703059f356ba9d7588bba45c5ed2d2e79d9f
Jonas Hietala <mail@jonashietala.se>
2021-10-14 12:25:09 +0200

    Note about error in test

--- test/bitpal/backends/mock/backend_mock_test.exs
+++ test/bitpal/backends/mock/backend_mock_test.exs
@@ -1,6 +1,12 @@
 defmodule BackendMockTest do
   use BitPal.IntegrationCase
 
+  # NOTE: This sometimes triggers the error:
+  #
+  # 12:21:21.210 [error] Postgrex.Protocol (#PID<0.662.0>) disconnected: ** (DBConnection.ConnectionError) client #PID<0.792.0> exited
+  #
+  # The tests still pass, but it's a bit annoying.
+  # I don't know why, but it still appears even if we stop_supervised!() everything else before Repo is exited.
   @tag backends: [
          {BitPal.BackendMock, auto: true, time_until_tx_seen: 10, time_between_blocks: 5}
        ]
