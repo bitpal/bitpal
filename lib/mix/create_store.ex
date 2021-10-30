b59298e899e839dc370b3c0042d3f6524bb48645
Jonas Hietala <mail@jonashietala.se>
2021-10-30 15:16:35 +0200

    Remove mix tasks, as they're not available in releases

--- lib/mix/create_store.ex
+++
@@ -1,9 +0,0 @@
-defmodule Mix.Tasks.Bitpal.Create.AccessToken do
-  use Mix.Task
-  import BitPalCli.Tasks
-  @dialyzer :no_undefined_callbacks
-
-  def run([store_id]) do
-    create_access_token(store_id)
-  end
-end
--- lib/mix/store_tokens.ex
+++
@@ -1,9 +0,0 @@
-defmodule Mix.Tasks.Bitpal.Store.Tokens do
-  use Mix.Task
-  import BitPalCli.Tasks
-  @dialyzer :no_undefined_callbacks
-
-  def run([store_id]) do
-    show_store_tokens(store_id)
-  end
-end
