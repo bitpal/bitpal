f2f38f5bf265a96c5093440b025497b1a16dcd88
Jonas Hietala <mail@jonashietala.se>
2023-05-05 20:42:08 +0200

    Weird type error on GitHub

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,5 @@
 [
   ~r/Mix.Task behaviour is not available/,
   ~r/Function .*Mix.+ does not exist/,
-  ~r/Function .*ExUnit.+ does not exist/,
-  ~r|Function doc_layout/2 has no local return|
+  ~r/Function .*ExUnit.+ does not exist/
 ]
